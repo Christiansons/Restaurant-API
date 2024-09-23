3219695421c8e2a15039d115c9d269092edab207
Alex <alexander.christianson@outlook.com>
2024-09-23 14:41:51 +0200

    Changed name for ReservationDTO to CreateReservationDTO to make different ones for get and post

--- Models/DTOs/CreateReservationDTO.cs
+++ Models/DTOs/CreateReservationDTO.cs
@@ -2,11 +2,10 @@
 {
 	public class CreateReservationDTO
 	{
+		public int ReservationNumber { get; set; }
 		public int PartySize { get; set; }
-		public string CustomerName { get; set; }
-		public string PhoneNr { get; set; }
 		public int TableNr { get; set; }
-		public DateTime timeFrom { get; set; } //Tänker att man får sitta i 2h så behöver inte ta emot sluttiden,
-		public DateTime timeTo { get; set; }
+		public int customerId { get; set; }
+		public DateTime timeFrom { get; set; }
 	}
 }
