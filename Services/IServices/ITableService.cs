2054401854617e5003a84cfe12c44406067e6431
Alex <alexander.christianson@outlook.com>
2024-09-23 20:35:09 +0200

    Changed update table arguments

--- Services/IServices/ITableService.cs
+++ Services/IServices/ITableService.cs
@@ -5,7 +5,7 @@ namespace Restaurant_API.Services.IServices
 	public interface ITableService
 	{
 		public Task CreateTable(int seats);
-		public Task<bool> UpdateTable(TableDTO tableDto);
+		public Task<bool> UpdateTable(int id, int seats);
 		public Task DeleteTable(int id);
 		public Task<TableDTO> GetTableByTableNr(int id);
 		public Task<IEnumerable<TableDTO>> GetAllTables();
